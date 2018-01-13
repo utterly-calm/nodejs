d26779ff9d0cadd314ba347b43c1b363baa319df
Manu Sharma <manu.sharma1222@gmail.com>
2018-01-13 20:42:12 +0530

    Projects page

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -47,6 +47,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
